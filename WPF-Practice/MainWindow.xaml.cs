43faed342d023edf601be3be9d506b63f9ca4c1f
Denys <133922338+denys-samoilov@users.noreply.github.com>
2024-12-18 13:19:54 +0200

    Додано додаткові мови та допрацьовано UI

--- WPF-Practice/MainWindow.xaml.cs
+++ WPF-Practice/MainWindow.xaml.cs
@@ -13,9 +13,6 @@ using System.Windows.Shapes;
 
 namespace WPF_Practice
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
